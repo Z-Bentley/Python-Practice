a79be15d92c9aec4e9003740043ec3194e2379b8
Zack Bentley <zpbentley99@gmail.com>
2024-07-20 22:24:02 -0400

    youtube downloader

--- Youtube-Downloader.py
+++ Youtube-Downloader.py
@@ -1,29 +1,7 @@
 # pytube practice downloading from youtube
-# from pytube import YouTube
 import yt_dlp as youtube_dl
 
 SAVE_PATH = 'C:/Users/ztben/Downloaded_Videos'
-
-# def Download(link):
-#     try:
-#         yt = YouTube(link)
-#         print(f'Title: {yt.title}')
-#         print(f'Author: {yt.author}')
-#         print(f'Length: {yt.length} seconds')
-
-#         # highResVid = yt.streams.filter(file_extension='mp4').get_highest_resolution()
-#         # streams = yt.streams.all()
-#         # for stream in streams:
-#         #     print(stream)
-#         print(yt.check_availability())
-    
-#         vid = yt.streams.first()
-#         print(f'Selected Stream: {vid}')
-    
-#         vid.download(output_path=SAVE_PATH)
-#         print('Download Complete!')
-#     except Exception as e:
-#         print(f'Error: {e}')
 
 def Download(link):
     try:
